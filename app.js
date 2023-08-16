14e14f4fb4ffa7c75b3072ef28e7e0e2d6413845
Cris180106 <cpimiento345@gmail.com>
2023-08-16 17:54:56 +0200

    numeros de las operaciones cambiadas

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const functions = require('./functions.js')
 
 console.log(functions);
 
-console.log(functions.add(5,3));
-console.log(functions.substract(2,8));
-console.log(functions.multiply(2,8));
+console.log(functions.add(8,26));
+console.log(functions.substract(789,98));
+console.log(functions.multiply(22,33));
 console.log(functions.divide(2,0.9));
